a5e800c5c27526225973af4c43e5ce81c08d3be8
Jonathan <jonathanmoorevla@gmail.com>
2023-02-18 09:49:33 -0600

    commit tree 8b351a8ef0df772c7a39e7429015db5c8bd61032

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, {length: {minimum: 250}}
+    validates :summary, {length: {maximum: 250}}
+    validates :category, {inclusion: {in: %w(Fiction Non-Fiction)}}
+
+    validate :needs_clickbait
+
+
+    MORE_CLICKBAIT = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+    def needs_clickbait
+        
+            if MORE_CLICKBAIT.none? {|clickbait| clickbait.match title }
+                # byebug
+                errors.add(:title, "Needs more clickbait")
+            end
+        
+    end
+
 end
